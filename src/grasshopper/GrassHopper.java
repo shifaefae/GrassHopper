7aaab4aec7f6547df973869ad0225c09d9b40976
shifa <nuriyahshifaaun@gmail.com>
2024-05-15 18:10:49 +0700

    hay semua

---
+++ src/grasshopper/GrassHopper.java
@@ -0,0 +1,15 @@
+
+public class GrassHopper {
+
+    
+    public static int Summation(int n) {
+        return n*(n+1)/2;
+       
+    }
+    public static void main(String[] args) {
+        System.out.println("Summation of 2 :" +Summation(2));
+        System.out.println("Summation of 8 :" +Summation(8));
+        System.out.println("Summation of 10 :" +Summation(10));
+    }
+    
+}
